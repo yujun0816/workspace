75dbfb63170759ad956e353d7e6ba30c10272e74
yujun <happyyujun0816@naver.com>
2025-01-07 11:47:47 +0900

    Lecture ch04

---
+++ ch04_operator/01_operator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,sum,sub,mul,inv;
+    a=10;
+    b=20;
+    sum=a+b;
+    sub=a-b;
+    mul=a*b;
+    inv=-a;
+
+    double apple;
+    int orange,banana;
+
+    apple=5.0/2.0;
+    banana=5/2;
+    orange=5%2;
+
+    printf("%.lf\n",apple);
+    printf("%d\n",banana);
+    printf("%d\n",orange);
+}
---
+++ ch04_operator/05_operator_casting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * 형 변환(Casting)
+ * 1.프로모션(small type -> big type)
+ * 
+ */
+int main(){
+    double res;
+    res *5;
+    printf("%.lf",res);
+}
