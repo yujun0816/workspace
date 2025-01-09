1fb05d1410e99c8cdf7d4cdb123b3d7b352fb3fa
yujun <happyyujun0816@naver.com>
2025-01-09 12:09:10 +0900

    Update Lecture ch99

---
+++ ch99_homework/03_number_sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//정수값 입력받기 ex)412
+//2
+//3. 출력 4+1+2=7
+int main(){
+    int num,a,sum=0;
+    printf("정수값:");
+    scanf("%d",&num);
+
+    while (num > 0) {
+        a = num % 10; 
+        sum += a;     
+        num /= 10;
+        if(num>=1)  
+            printf("%d + ",a);
+        else
+            printf("%d = %d",a,sum);
+
+    }
+
+}
